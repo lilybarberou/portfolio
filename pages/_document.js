a4142d800e6034e60f964ea135624e0a49452221
Lily <hayashi.quan@gmail.com>
2022-10-22 23:42:27 +0200

    fix - ssr management for react-jss

---
+++ pages/_document.js
@@ -0,0 +1,53 @@
+import Document, { Html, Head, Main, NextScript } from 'next/document';
+import { SheetsRegistry, JssProvider, createGenerateId } from 'react-jss';
+
+class MyDocument extends Document {
+    static async getInitialProps(ctx) {
+        const registry = new SheetsRegistry();
+        const generateId = createGenerateId();
+        const originalRenderPage = ctx.renderPage;
+
+        ctx.renderPage = () =>
+            originalRenderPage({
+                enhanceApp: (App) => (props) =>
+                    (
+                        <JssProvider registry={registry} generateId={generateId}>
+                            <App {...props} />
+                        </JssProvider>
+                    ),
+            });
+
+        const initialProps = await Document.getInitialProps(ctx);
+        return {
+            ...initialProps,
+            styles: (
+                <>
+                    {initialProps.styles}
+                    <style id="server-side-styles">{registry.toString()}</style>
+                </>
+            ),
+        };
+    }
+    render() {
+        return (
+            <Html>
+                <Head>
+                    <meta charSet="utf-8" />
+                    {/* <meta name="viewport" content="width=device-width, initial-scale=1" /> */}
+                    <meta name="theme-color" content="#232323" />
+                    <link rel="manifest" href="/manifest.json" />
+                    <link rel="icon" href="/favicon.ico" />
+                    <link rel="apple-touch-icon" href="./apple-touch-icon.png" />
+
+                    <meta property="og:image" content="https://raw.githubusercontent.com/Nahay/Assets/master/Selfportrait/banner.png" />
+                    <meta property="og:type" content="website" />
+                </Head>
+                <body>
+                    <Main />
+                    <NextScript />
+                </body>
+            </Html>
+        );
+    }
+}
+export default MyDocument;
