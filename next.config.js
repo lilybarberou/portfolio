995f7a1e0e6035c8426ac8f5bc68e40c6b29e3a2
Lily <hayashi.quan@gmail.com>
2022-10-22 23:31:51 +0200

    build - setup files & libraries

--- next.config.js
+++ next.config.js
@@ -1,7 +1,21 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {
-  reactStrictMode: true,
-  swcMinify: true,
-}
+// /** @type {import('next').NextConfig} */
+// const nextConfig = {
+//     reactStrictMode: true,
+//     swcMinify: true,
+// };
 
-module.exports = nextConfig
+// module.exports = nextConfig;
+
+
+module.exports = {
+    webpack(config) {
+      config.module.rules.push({
+        test: /\.svg$/,
+        use: ["@svgr/webpack"]
+      });
+  
+      return config;
+    },
+    swcMinify: true,
+    reactStrictMode: true,
+  };
