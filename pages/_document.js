dd12dcbbae0e52fbb1d4a7d5f218ec6fcecd0eae
Lily <hayashi.quan@gmail.com>
2022-10-24 14:15:02 +0200

    fix - move viewport meta in _app

--- pages/_document.js
+++ pages/_document.js
@@ -33,7 +33,6 @@ class MyDocument extends Document {
             <Html>
                 <Head>
                     <meta charSet="utf-8" />
-                    {/* <meta name="viewport" content="width=device-width, initial-scale=1" /> */}
                     <meta name="theme-color" content="#232323" />
                     <link rel="manifest" href="/manifest.json" />
                     <link rel="icon" href="/favicon.ico" />
