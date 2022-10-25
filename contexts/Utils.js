a39845aef7bd869d64f7007b7fb54be7c1255b6c
Lily <hayashi.quan@gmail.com>
2022-10-25 16:35:39 +0200

    feat - add contact page

--- contexts/Utils.js
+++ contexts/Utils.js
@@ -9,6 +9,17 @@ export const renderHtml = (el, translation) => {
     document.querySelector(el).innerHTML = translation;
 };
 
+export const getFormData = (el) => {
+    const form = document.querySelector(el);
+    const data = new FormData(form);
+
+    const obj = {};
+    for (const i of data.entries()) {
+        obj[i[0]] = i[1];
+    }
+    return obj;
+};
+
 export default class Parallax {
     constructor(data) {
         this.target = document.querySelector(data.target);
