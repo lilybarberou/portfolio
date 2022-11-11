968e3641dae4277e1a898513663e11c09b682919
Lily <hayashi.quan@gmail.com>
2022-11-11 17:48:28 +0100

    add - 404 page

---
+++ pages/404.js
@@ -0,0 +1,39 @@
+import { createUseStyles } from 'react-jss';
+
+const useStyle = createUseStyles({
+    container: {
+        display: 'flex',
+        flexDirection: 'column',
+        fontSize: 100,
+        gap: 20,
+
+        '& > video': {
+            width: '100%',
+        },
+
+        '@media(min-width: 600px)': {
+            height: '100%',
+            justifyContent: 'center',
+            marginLeft: 100,
+
+            '& > video': {
+                width: 500,
+            },
+        },
+    },
+});
+
+const NotFound = () => {
+    const classes = useStyle();
+
+    return (
+        <div className={classes.container}>
+            <span>404</span>
+            <video muted autoPlay={true} loop={true}>
+                <source src="https://i.imgur.com/6NdzIG0.mp4" />
+            </video>
+        </div>
+    );
+};
+
+export default NotFound;
