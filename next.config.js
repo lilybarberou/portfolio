779c30a3acb6c63af8123bbb4e61f09153ee5f15
Lily <hayashi.quan@gmail.com>
2022-12-17 18:11:54 +0100

    fix - pwa

--- next.config.js
+++ next.config.js
@@ -1,15 +1,19 @@
-module.exports = {
+const withPWA = require('next-pwa')({
+    dest: 'public',
+});
+
+module.exports = withPWA({
     webpack(config) {
-      config.module.rules.push({
-        test: /\.svg$/,
-        use: ["@svgr/webpack"]
-      });
-  
-      return config;
+        config.module.rules.push({
+            test: /\.svg$/,
+            use: ['@svgr/webpack'],
+        });
+
+        return config;
     },
     swcMinify: true,
     reactStrictMode: true,
     images: {
-      domains: ["raw.githubusercontent.com"]
-    }
-  };
+        domains: ['raw.githubusercontent.com'],
+    },
+});
