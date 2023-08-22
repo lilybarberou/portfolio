c3fa5fbca3dede3fa1925b6d26fd923e5e965fe4
Lily <hayashi.quan@gmail.com>
2023-08-22 22:40:49 +0200

    update - remove strategy from gtag script

--- pages/_app.jsx
+++ pages/_app.jsx
@@ -20,8 +20,8 @@ function MyApp({ Component, pageProps }) {
 
     return (
         <>
-            <Script src={`https://www.googletagmanager.com/gtag/js?id=${process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS}`} strategy='afterInteractive' />
-            <Script id='google-analytics' strategy='afterInteractive'>
+            <Script src={`https://www.googletagmanager.com/gtag/js?id=${process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS}`} />
+            <Script id='google-analytics'>
                 {`
                     window.dataLayer = window.dataLayer || [];
                     function gtag(){window.dataLayer.push(arguments);}
